3c89c4c7c7a51651af4266701cc286092c94ea36
seba gomez <sebagomezcorrea@outlook.com>
2018-02-26 16:37:29 -0300

    the index helper project is now a .net core component

---
+++ src/common/GoomezIndexHelper/User.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace GoomezIndexHelper
+{
+	public class User
+	{
+		public string Name { get; internal set; }
+
+		public User(string user)
+		{
+			if (user.Contains("\\"))
+				Name = user.Substring(user.LastIndexOf("\\") + 1);
+			else
+				Name = user;
+		}
+
+		public override string ToString()
+		{
+			return Name;
+		}
+	}
+}
